55f288242c80c0c5493c4ce56bfc81645a494c83
Pedro Janeo <pdro.henriquejanneo@gmail.com>
2024-09-04 13:13:49 -0300

    feat: atualização da aula do dia 04/09

---
+++ js/dados.js
@@ -0,0 +1,62 @@
+let personagens = [
+    {
+      nome: "Zoro",
+      descricao: "Imediato",
+      link: "https://onepiece.fandom.com/pt/wiki/Roronoa_Zoro"
+    },
+    {
+      nome: "Luffy",
+      descricao: "Capitão",
+      link: "https://onepiece.fandom.com/pt/wiki/Monkey_D._Luffy"
+    },
+    {
+      nome: "Sanji",
+      descricao: "Cozinheiro",
+      link: "https://onepiece.fandom.com/pt/wiki/Sanji"
+    },
+    {
+      nome: "Jimbe",
+      descricao: "Timoneiro",
+      link: "https://onepiece.fandom.com/pt/wiki/Jimbe"
+    },
+
+    {
+      nome: "Nami",
+      descricao: "Navegadora",
+      link: "https://onepiece.fandom.com/pt/wiki/Nami"
+    },
+    {
+      nome: "Usopp",
+      descricao: "Atirador",
+      link: "https://onepiece.fandom.com/pt/wiki/Usopp"
+    },
+    {
+      nome: "Chopper",
+      descricao: "Médico",
+      link: "https://onepiece.fandom.com/pt/wiki/Tony_Tony_Chopper"
+    },
+    {
+      nome: "Robin",
+      descricao: "Arqueóloga",
+      link: "https://onepiece.fandom.com/pt/wiki/Nico_Robin"
+    },
+    {
+      nome: "Franky",
+      descricao: "Carpinteiro",
+      link: "https://onepiece.fandom.com/pt/wiki/Franky"
+    },
+    {
+        nome: "Brook",
+        descricao: "Musico",
+        link:"https://onepiece.fandom.com/pt/wiki/Brook"
+    }
+  ];
+// [ ] --> lista (array)
+// {} --> objeto
+/*  
+[{
+}]
+    --> lista de objetos
+
+*/
+
