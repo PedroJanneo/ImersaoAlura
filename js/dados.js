f73ca936cb42f5e6818e141e1ee261f017d6515f
Pedro Janeo <pdro.henriquejanneo@gmail.com>
2024-09-06 10:33:51 -0300

    feats: funcionalidade de pesquisa universal

--- js/dados.js
+++ js/dados.js
@@ -2,52 +2,62 @@ let personagens = [
   {
     nome: "Monkey D. Luffy",
     descricao: "O capitão dos Piratas do Chapéu de Palha e protagonista da história. Seu sonho é se tornar o Rei dos Piratas.",
-    link: "https://onepiece.fandom.com/pt/wiki/Monkey_D._Luffy"
+    link: "https://onepiece.fandom.com/pt/wiki/Monkey_D._Luffy",
+    tags: 'capitao nika gear'
 },
 {
     nome: "Roronoa Zoro",
     descricao: "O espadachim dos Chapéus de Palha. Seu sonho é se tornar o maior espadachim do mundo.",
-    link: "https://onepiece.fandom.com/pt/wiki/Roronoa_Zoro"
+    link: "https://onepiece.fandom.com/pt/wiki/Roronoa_Zoro",
+    tags: 'imediato solador'
 },
 {
     nome: "Nami",
     descricao: "A navegadora dos Chapéus de Palha. Seu amor pelos mapas e seu desejo de desenhar um mapa completo do mundo a impulsionam em suas aventuras.",
-    link: "https://onepiece.fandom.com/pt/wiki/Nami"
+    link: "https://onepiece.fandom.com/pt/wiki/Nami",
+    tags: 'navegadora laranja'
 },
 {
     nome: "Usopp",
     descricao: "O atirador dos Chapéus de Palha. Um jovem com uma imaginação fértil e um grande coração.",
-    link: "https://onepiece.fandom.com/pt/wiki/Usopp"
+    link: "https://onepiece.fandom.com/pt/wiki/Usopp",
+    tags: 'mira boa medroso'
 },
 {
     nome: "Tony Tony Chopper",
     descricao: "A rena do Chapéu de Palha e médico da tripulação. Comeu a Fruta do Diabo Hito Hito no Mi, modelo humano.",
-    link: "https://onepiece.fandom.com/pt/wiki/Tony_Tony_Chopper"
+    link: "https://onepiece.fandom.com/pt/wiki/Tony_Tony_Chopper",
+    tags: "doce fofo"
 },
 {
     nome: "Nico Robin",
     descricao: "A arqueóloga dos Chapéus de Palha. Uma mulher misteriosa com a habilidade de fazer florescer partes de seu corpo.",
-    link: "https://onepiece.fandom.com/pt/wiki/Nico_Robin"
+    link: "https://onepiece.fandom.com/pt/wiki/Nico_Robin",
+    tags: " leitora esperta"
 },
 {
     nome: "Franky",
     descricao: "O carpinteiro dos Chapéus de Palha. Um ciborgue com um grande amor por navios.",
-    link: "https://onepiece.fandom.com/pt/wiki/Franky"
+    link: "https://onepiece.fandom.com/pt/wiki/Franky",
+    tags: "construtor navio"
 },
 {
     nome: "Brook",
     descricao: "O músico dos Chapéus de Palha. Um esqueleto que comeu a Fruta do Diabo Yomi Yomi no Mi.",
-    link: "https://onepiece.fandom.com/pt/wiki/Brook"
+    link: "https://onepiece.fandom.com/pt/wiki/Brook",
+    tags: "musico violao violão"
 },
 {
     nome: "Vinsmoke Sanji",
     descricao: "O cozinheiro dos Chapéus de Palha. Conhecido por sua lealdade aos amigos, seu amor por mulheres bonitas e suas poderosas pernas.",
-    link: "https://onepiece.fandom.com/pt/wiki/Sanji"
+    link: "https://onepiece.fandom.com/pt/wiki/Sanji",
+    tags: " comida gado amor"
 },
 {
     nome: "Jimbe",
     descricao: "O timoneiro dos Chapéus de Palha. Um homem-peixe sábio e poderoso.",
-    link: "https://onepiece.fandom.com/pt/wiki/Jimbe"
+    link: "https://onepiece.fandom.com/pt/wiki/Jimbe",
+    tags: "dirigir pilotar nadador"
 }
 ];
 // [ ] --> lista (array)
--- js/main.js
+++ js/main.js
@@ -1,16 +1,27 @@
 function pesquisa() {
-    // Seleciona o elemento HTML com o ID "resultados-pesquisa" para exibir os resultados.
-    let sessao = document.getElementById("resultados-pesquisa");
-  
-    // Seleciona o elemento HTML com o ID "pesquisa" (não utilizado nesta função).
-    let p = document.getElementById("pesquisa");
-  
-    // Inicializa uma string vazia para armazenar os resultados da pesquisa.
-    let resultado = '';
-  
-    // Itera sobre cada personagem no array "personagens".
-    for (let personagem of personagens) {
-      // Constrói o HTML para cada item do resultado da pesquisa.
+  // Seleciona a seção onde os resultados serão exibidos
+  let sessao = document.getElementById("resultados-pesquisa");
+
+  // Obtém o termo de pesquisa (não utilizado nesta função)
+  let termoPesquisa = document.getElementById("pesquisa").value;
+
+  // Inicializa a string para armazenar os resultados
+  let resultado = '';
+  termoPesquisa = termoPesquisa.toLowerCase();
+  // Verifica se o termo de pesquisa está vazio
+  if (!termoPesquisa ) {
+    sessao.innerHTML = "<p>Nenhum personagem encontrado. Você não pesquisou nenhum personagem ou caracteristica</p>";
+    return;
+  }
+let nome = '';
+let descricao = '';
+let tags = '';
+  // Itera sobre os personagens e constrói o HTML dos resultados
+  for (let personagem of personagens) {
+      nome = personagem.nome.toLowerCase();
+      descricao = personagem.descricao.toLowerCase();
+      tags = personagem.tags.toLowerCase();
+    if (nome.includes(termoPesquisa) || descricao.includes(termoPesquisa) || tags.includes(termoPesquisa)) {
       resultado += `
         <div class="item-resultado">
           <h2>
@@ -21,7 +32,11 @@ function pesquisa() {
         </div>
       `;
     }
-  
-    // Atribui o HTML gerado ao elemento "sessao", atualizando a página com os resultados.
-    sessao.innerHTML = resultado;
   }
+
+    if ( !resultado ){
+      resultado = '<p> nada foi encontrado</p>'
+    }
+  // Atualiza a seção com os resultados
+  sessao.innerHTML = resultado;
+}
