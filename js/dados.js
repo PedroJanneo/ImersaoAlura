269e991916be25b22b48d13db626b72b15e589ec
Pedro Janeo <pdro.henriquejanneo@gmail.com>
2024-09-06 11:18:31 -0300

    feat: adicionando novas tags

--- js/dados.js
+++ js/dados.js
@@ -1,65 +1,66 @@
 let personagens = [
-  {
-    nome: "Monkey D. Luffy",
-    descricao: "O capitão dos Piratas do Chapéu de Palha e protagonista da história. Seu sonho é se tornar o Rei dos Piratas.",
-    link: "https://onepiece.fandom.com/pt/wiki/Monkey_D._Luffy",
-    tags: 'capitao nika gear'
-},
-{
-    nome: "Roronoa Zoro",
-    descricao: "O espadachim dos Chapéus de Palha. Seu sonho é se tornar o maior espadachim do mundo.",
-    link: "https://onepiece.fandom.com/pt/wiki/Roronoa_Zoro",
-    tags: 'imediato solador'
-},
-{
-    nome: "Nami",
-    descricao: "A navegadora dos Chapéus de Palha. Seu amor pelos mapas e seu desejo de desenhar um mapa completo do mundo a impulsionam em suas aventuras.",
-    link: "https://onepiece.fandom.com/pt/wiki/Nami",
-    tags: 'navegadora laranja'
-},
-{
-    nome: "Usopp",
-    descricao: "O atirador dos Chapéus de Palha. Um jovem com uma imaginação fértil e um grande coração.",
-    link: "https://onepiece.fandom.com/pt/wiki/Usopp",
-    tags: 'mira boa medroso'
-},
-{
-    nome: "Tony Tony Chopper",
-    descricao: "A rena do Chapéu de Palha e médico da tripulação. Comeu a Fruta do Diabo Hito Hito no Mi, modelo humano.",
-    link: "https://onepiece.fandom.com/pt/wiki/Tony_Tony_Chopper",
-    tags: "doce fofo"
-},
-{
-    nome: "Nico Robin",
-    descricao: "A arqueóloga dos Chapéus de Palha. Uma mulher misteriosa com a habilidade de fazer florescer partes de seu corpo.",
-    link: "https://onepiece.fandom.com/pt/wiki/Nico_Robin",
-    tags: " leitora esperta"
-},
-{
-    nome: "Franky",
-    descricao: "O carpinteiro dos Chapéus de Palha. Um ciborgue com um grande amor por navios.",
-    link: "https://onepiece.fandom.com/pt/wiki/Franky",
-    tags: "construtor navio"
-},
-{
-    nome: "Brook",
-    descricao: "O músico dos Chapéus de Palha. Um esqueleto que comeu a Fruta do Diabo Yomi Yomi no Mi.",
-    link: "https://onepiece.fandom.com/pt/wiki/Brook",
-    tags: "musico violao violão"
-},
-{
-    nome: "Vinsmoke Sanji",
-    descricao: "O cozinheiro dos Chapéus de Palha. Conhecido por sua lealdade aos amigos, seu amor por mulheres bonitas e suas poderosas pernas.",
-    link: "https://onepiece.fandom.com/pt/wiki/Sanji",
-    tags: " comida gado amor"
-},
-{
-    nome: "Jimbe",
-    descricao: "O timoneiro dos Chapéus de Palha. Um homem-peixe sábio e poderoso.",
-    link: "https://onepiece.fandom.com/pt/wiki/Jimbe",
-    tags: "dirigir pilotar nadador"
-}
-];
+    {
+      nome: "Monkey D. Luffy",
+      descricao: "O capitão dos Piratas do Chapéu de Palha e protagonista da história. Seu sonho é se tornar o Rei dos Piratas.",
+      link: "https://onepiece.fandom.com/pt/wiki/Monkey_D._Luffy",
+      tags: 'capitao nika gear elastico sonhador aventureiro líder destemido'
+    },
+    {
+      nome: "Roronoa Zoro",
+      descricao: "O espadachim dos Chapéus de Palha. Seu sonho é se tornar o maior espadachim do mundo.",
+      link: "https://onepiece.fandom.com/pt/wiki/Roronoa_Zoro",
+      tags: 'espada espadachim imediato solador persistente forte estrategista'
+    },
+    {
+      nome: "Nami",
+      descricao: "A navegadora dos Chapéus de Palha. Seu amor pelos mapas e seu desejo de desenhar um mapa completo do mundo a impulsionam em suas aventuras.",
+      link: "https://onepiece.fandom.com/pt/wiki/Nami",
+      tags: 'navegadora laranja cartografia inteligente astuta ambiciosa'
+    },
+    {
+      nome: "Usopp",
+      descricao: "O atirador dos Chapéus de Palha. Um jovem com uma imaginação fértil e um grande coração.",
+      link: "https://onepiece.fandom.com/pt/wiki/Usopp",
+      tags: 'atirador mentiroso corajoso imaginativo inventor sonhador'
+    },
+    {
+      nome: "Tony Tony Chopper",
+      descricao: "A rena do Chapéu de Palha e médico da tripulação. Comeu a Fruta do Diabo Hito Hito no Mi, modelo humano.",
+      link: "https://onepiece.fandom.com/pt/wiki/Tony_Tony_Chopper",
+      tags: "médico rena doçura transformação fofo compaixão habilidade"
+    },
+    {
+      nome: "Nico Robin",
+      descricao: "A arqueóloga dos Chapéus de Palha. Uma mulher misteriosa com a habilidade de fazer florescer partes de seu corpo.",
+      link: "https://onepiece.fandom.com/pt/wiki/Nico_Robin",
+      tags: "arqueóloga poder florescimento intelectual misteriosa eloquente profunda"
+    },
+    {
+      nome: "Franky",
+      descricao: "O carpinteiro dos Chapéus de Palha. Um ciborgue com um grande amor por navios.",
+      link: "https://onepiece.fandom.com/pt/wiki/Franky",
+      tags: "carpinteiro ciborgue engenheiro navio criativo construtor robusto"
+    },
+    {
+      nome: "Brook",
+      descricao: "O músico dos Chapéus de Palha. Um esqueleto que comeu a Fruta do Diabo Yomi Yomi no Mi.",
+      link: "https://onepiece.fandom.com/pt/wiki/Brook",
+      tags: "músico esqueleto violão morto-vivo animado carismático imortal"
+    },
+    {
+      nome: "Vinsmoke Sanji",
+      descricao: "O cozinheiro dos Chapéus de Palha. Conhecido por sua lealdade aos amigos, seu amor por mulheres bonitas e suas poderosas pernas.",
+      link: "https://onepiece.fandom.com/pt/wiki/Sanji",
+      tags: "cozinheiro pernas poderosas lealdade galanteador chef habilidoso lutador"
+    },
+    {
+      nome: "Jimbe",
+      descricao: "O timoneiro dos Chapéus de Palha. Um homem-peixe sábio e poderoso.",
+      link: "https://onepiece.fandom.com/pt/wiki/Jimbe",
+      tags: "timoneiro homem-peixe sábio forte nadador protetor diplomata experiente"
+    }
+  ];
+  
 // [ ] --> lista (array)
 // {} --> objeto
 /*  
